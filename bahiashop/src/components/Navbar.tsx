64a237dfc774f3ab26292051c4655cb00cec58dc
santiMercado0694 <santimercado06_94@hotmail.com>
2024-05-17 10:41:25 -0300

    Diseño incial del cart y lo agregue al navbar

--- bahiashop/src/components/Navbar.tsx
+++ bahiashop/src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import MaxWidthWrapper from "./MaxWidthWrapper"
 import { Icons } from "./Icons"
 import NavItems from "./NavItems"
 import { buttonVariants } from "./ui/button"
+import Cart from "./Cart"
 
 const Navbar = () => {
 
@@ -75,7 +76,7 @@ const Navbar = () => {
                                     )}
 
                                     <div className="ml-4 flow-root lg:ml-6">
-                                        
+                                        <Cart />
                                     </div>
                                 </div>
                             </div>
