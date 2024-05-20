53a32bfe81da7da0fbf0d3de3c64276d9b5b04ea
santiMercado0694 <santimercado06_94@hotmail.com>
2024-05-20 17:57:36 -0300

    agregue Next Auth a la app

--- bahiashop/src/app/layout.tsx
+++ bahiashop/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import Navbar from "@/components/Navbar";
+import SessionWrapper from "@/components/SessionWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,19 +18,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full">
-      <body 
-        className={cn(
-          "relative h-full font-sans antialiased",
-          inter.className
-        )}>
-        <main className="relative flex flex-col min-h-screen"> 
-          <Navbar/>
-          <div className="flex-grow flex-1">
-            {children}
-          </div>
-        </main>
-      </body>
-    </html>
+    <SessionWrapper>
+      <html lang="en" className="h-full">
+        <body 
+          className={cn(
+            "relative h-full font-sans antialiased",
+            inter.className
+          )}>
+          <main className="relative flex flex-col min-h-screen"> 
+            <Navbar/>
+            <div className="flex-grow flex-1">
+              {children}
+            </div>
+          </main>
+        </body>
+      </html>
+    </SessionWrapper>
   );
 }
