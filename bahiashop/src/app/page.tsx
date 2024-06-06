59b48ee2c8b02caf95d59ba8b25359a32dc8ab2b
santiMercado0694 <santimercado06_94@hotmail.com>
2024-06-06 11:48:36 -0300

    volvi atras el cambio

--- bahiashop/src/app/page.tsx
+++ bahiashop/src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
   useEffect(() => {
     getProductsFromAPI();
     getCategories();
-  }, [getProductsFromAPI, getCategories]);
+  }, []);
 
   return (
     <MaxWidthWrapper>
