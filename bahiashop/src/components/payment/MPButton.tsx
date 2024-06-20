5adf0f9cea85646b4d9b457b79007142a68c3d71
Lucas González <lucasgxnzalez@gmail.com>
2024-06-20 20:30:03 -0300

    mp fix

--- bahiashop/src/components/payment/MPButton.tsx
+++ bahiashop/src/components/payment/MPButton.tsx
@@ -3,16 +3,21 @@ import {useGlobalContext} from "@/context/StoreProvider";
 import {usePaymentContext} from "@/context/MPProvider";
 import Box from "@mui/material/Box";
 import { initMercadoPago, Wallet } from '@mercadopago/sdk-react'
+import {useRouter} from "next/navigation";
+import {useSession} from "next-auth/react";
 
 
 export default function MPButton() {
   
-  const {cart} = useGlobalContext();
+  const router = useRouter();
+  const { data: session } = useSession();
+  const {cart, clearCartByUserId} = useGlobalContext();
   const {getPreferenceId} = usePaymentContext();
   const [prefRequest, setPrefRequest] = useState<boolean>(false);
   
   
   const [preferenceId, setPreferenceId] = useState(null);
+  const [paymentUrl, setPaymentUrl] = useState(null);
   
   
   useEffect(() => {
@@ -34,18 +39,27 @@ export default function MPButton() {
     
     getPreferenceId(items).then((result : any) => {
       setPreferenceId(result.id);
+      setPaymentUrl(result.init_point);
     });
     setPrefRequest(true)
   }, [cart]);
+  
+  function goHome() {
+    if(!session) return;
+    clearCartByUserId(session.user.user_id).then(() => {
+      router.push('/');
+      router.refresh();
+    })
+    
+  }
   
   
   return (
     // <Box component="form" noValidate  id={FORM_ID} method="GET" />
     <Box id="wallet_container">
-      { preferenceId ?
+      { paymentUrl ?
         
-        <Wallet initialization={{ preferenceId: preferenceId }} />
-        // <a href={payURL??'#'}> Ir a pagar </a>
+        <a role="button" className="btn btn-primary align-middle" target="_blank" href={paymentUrl} onClick={goHome} > Ir a pagar </a>
         
         :
         
